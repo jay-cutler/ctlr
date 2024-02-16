15c89459224773b86fc8468511563d653393b984
jay-cutler <jcutler@michiganfitness.org>
2024-02-16 15:32:56 -0500

    Try switching quotes and non-quotes in comparison_table()

--- R/comparison_table.R
+++ R/comparison_table.R
@@ -5,7 +5,7 @@
 #' @param .data The data frame.
 #' @param comparison_variable Variable to compare response distribution across time points.
 #' @param new_name Final name to display for the left (variable) column in the table. If not entered, will default to a sentence case version of the `comparison_variable`.
-#' @param time_point Name of time point variable in dataset. Defaults to "fy".
+#' @param time_point Name of time point variable in dataset. Defaults to "fy" (without quotes).
 #' @param cap_times Do you want to force the time point columns to be renamed as all caps (T/F)? Defaults to TRUE.
 #' @param data_format Format of data presented in table. Can be one of "n" (only display n), "pct" (only display percent), or "n_pct" (display both n and percent; default).
 #'
@@ -15,7 +15,7 @@
 #'
 #' @export
 comparison_table <- function(.data, comparison_variable, new_name = NULL,
-                             time_point = "fy", cap_times = TRUE, data_format = "n_pct") {
+                             time_point = fy, cap_times = TRUE, data_format = "n_pct") {
 
   if (!data_format %in% c("n", "pct", "n_pct")) {
     stop("'data_format' argument must be one of: 'n','pct', 'n_pct'")
@@ -27,19 +27,19 @@ comparison_table <- function(.data, comparison_variable, new_name = NULL,
     var_name <- new_name
   }
 
-  time_point_var <- noquote(time_point)
+  time_point_string <- deparse(substitute({{ time_point }}))
 
   if(data_format == "n_pct"){
     totals <- .data |>
       tidyr::drop_na({{ comparison_variable }}) |>
-      janitor::tabyl({{ comparison_variable }}, time_point_var) |>
+      janitor::tabyl({{ comparison_variable }}, {{ time_point }}) |>
       janitor::adorn_totals(where = "row") |>
       dplyr::filter({{ comparison_variable }} == "Total") |>
       dplyr::mutate(dplyr::across(-{{ comparison_variable }}, ~paste(.x, "(100%)", sep = " ")))
 
     table <- .data |>
       tidyr::drop_na({{ comparison_variable }}) |>
-      dplyr::group_by(time_point_var) |>
+      dplyr::group_by({{ time_point }}) |>
       dplyr::count({{ comparison_variable }}) |>
       dplyr::mutate(pct = n/sum(n))  |>
       janitor::adorn_pct_formatting(digits = 0,
@@ -49,7 +49,7 @@ comparison_table <- function(.data, comparison_variable, new_name = NULL,
       dplyr::mutate(n_pct = paste(n, " (", pct, ")", sep = "")) |>
       dplyr::select(-c(n, pct)) |>
       dplyr::ungroup() |>
-      tidyr::pivot_wider(names_from = {{ time_point }},
+      tidyr::pivot_wider(names_from = time_point_string,
                          values_from = "n_pct") |>
       dplyr::arrange({{ comparison_variable }}) |>
       dplyr::bind_rows(totals)
@@ -59,7 +59,7 @@ comparison_table <- function(.data, comparison_variable, new_name = NULL,
 
     table <- .data |>
       tidyr::drop_na({{ comparison_variable }}) |>
-      janitor::tabyl({{ comparison_variable }}, time_point_var) |>
+      janitor::tabyl({{ comparison_variable }}, {{ time_point }}) |>
       janitor::adorn_percentages(denominator = "col") |>
       janitor::adorn_pct_formatting(rounding = "half to even",
                            digits = 0) |>
@@ -70,10 +70,10 @@ comparison_table <- function(.data, comparison_variable, new_name = NULL,
   if(data_format == "n"){
     table <- .data |>
       tidyr::drop_na({{ comparison_variable }}) |>
-      dplyr::group_by(time_point_var) |>
+      dplyr::group_by({{ time_point }}) |>
       dplyr::count({{ comparison_variable }}) |>
       dplyr::ungroup() |>
-      tidyr::pivot_wider(names_from = {{ time_point }},
+      tidyr::pivot_wider(names_from = time_point_string,
                          values_from = "n") |>
       dplyr::arrange({{ comparison_variable }}) |>
       janitor::adorn_totals(where = "row")
