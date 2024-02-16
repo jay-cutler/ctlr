dc335cd2d223c0eb83f70b17dbf5280a8aa76290
jay-cutler <jcutler@michiganfitness.org>
2024-02-16 12:30:47 -0500

    Fix typo

--- R/ctlr.R
+++ R/ctlr.R
@@ -1,4 +1,4 @@
-#' cltr: Shortcuts for data cleaning, analysis, and visualization
+#' ctlr: Shortcuts for data cleaning, analysis, and visualization
 #'
 #' This package utilizes functions from the {tidyverse} as well as {flextable}, {qualtRics}, and {boxr} to offer function "shortcuts" for simple data cleaning, wrangling similar types of survey data, visualizing data in tabular and graphical forms, and connecting and utilizing APIs.
 #'
